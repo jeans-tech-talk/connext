af65f18cdac7541386680b97e2c605c19ca79c8d
Rujikorn Khampho <jean@macbook-air.local>
2023-03-12 10:20:07 +0700

    refactor: inherit user base serializer

--- backend/core/apps/authentication/serializers.py
+++ backend/core/apps/authentication/serializers.py
@@ -1,3 +1,4 @@
+from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 
@@ -6,20 +7,12 @@ from core.apps.finance.models import BankAccount
 from core.apps.finance.serializers import BankAccountCreateSerializer, BankAccountReadSerializer
 
 
-class UserCreateSerializer(serializers.ModelSerializer):
-    bank_account = BankAccountCreateSerializer(required=False)
-    confirm_password = serializers.CharField(write_only=True)
-    email = serializers.EmailField(validators=[UniqueValidator(queryset=User.objects.all())])
-    id_card_number = serializers.CharField(validators=[UniqueValidator(queryset=User.objects.all())])
-    password = serializers.CharField(write_only=True)
-    phone_number = serializers.CharField(validators=[UniqueValidator(queryset=User.objects.all())])
-
+class UserBaseSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         exclude = [
             'date_joined',
             'groups',
-            'id',
             'is_active',
             'is_staff',
             'is_superuser',
@@ -27,9 +20,21 @@ class UserCreateSerializer(serializers.ModelSerializer):
             'user_permissions',
         ]
 
+
+class UserCreateSerializer(UserBaseSerializer):
+    bank_account = BankAccountCreateSerializer(required=False)
+    confirm_password = serializers.CharField(write_only=True)
+    email = serializers.EmailField(validators=[UniqueValidator(queryset=User.objects.all())])
+    id_card_number = serializers.CharField(validators=[UniqueValidator(queryset=User.objects.all())])
+    password = serializers.CharField(write_only=True)
+    phone_number = serializers.CharField(validators=[UniqueValidator(queryset=User.objects.all())])
+
+    class Meta(UserBaseSerializer.Meta):
+        exclude = ['id'] + UserBaseSerializer.Meta.exclude
+
     def validate(self, data):
         if data.get('password') != data.pop('confirm_password', None):
-            raise serializers.ValidationError({'password': 'Password did not match'})
+            raise serializers.ValidationError(_('Password did not match'))
         return data
 
     def create(self, validated_data):
@@ -40,18 +45,8 @@ class UserCreateSerializer(serializers.ModelSerializer):
         return user
 
 
-class UserReadSerializer(serializers.ModelSerializer):
+class UserReadSerializer(UserBaseSerializer):
     bank_account = BankAccountReadSerializer()
 
-    class Meta:
-        model = User
-        exclude = [
-            'date_joined',
-            'groups',
-            'is_active',
-            'is_staff',
-            'is_superuser',
-            'last_login',
-            'password',
-            'user_permissions',
-        ]
+    class Meta(UserBaseSerializer.Meta):
+        exclude = ['password'] + UserBaseSerializer.Meta.exclude
