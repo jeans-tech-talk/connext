9d0542f225c0553b25f1ca9dfb95460f0bdddaff
Rujikorn Khampho <jean@macbook-air.local>
2023-03-15 08:52:31 +0700

    feat: initial bank account model

---
+++ backend/core/apps/finance/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+from core.apps.finance.choices import AccountType
+
+
+class BankAccount(models.Model):
+    account_name = models.CharField(max_length=150)
+    account_number = models.CharField(max_length=150)
+    account_type = models.CharField(max_length=15, choices=AccountType.choices)
+    bank = models.CharField(max_length=150)
+    branch = models.CharField(max_length=150)
+    user = models.OneToOneField(
+        'authentication.CustomUser',
+        on_delete=models.CASCADE,
+        related_name='bank_account',
+    )
+
+    objects = models.Manager()
+
+    class Meta:
+        db_table = 'finance_bank_account'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['account_number', 'bank'],
+                name='unique_bank_account',
+            ),
+        ]
