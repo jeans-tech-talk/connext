0edf02e59b2473ddc880fa78aa1b90aedb916cfd
Rujikorn Khampho <jean@macbook-air.local>
2023-03-15 09:11:55 +0700

    refactor: mask text as a translation string

--- backend/core/apps/authentication/models.py
+++ backend/core/apps/authentication/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 from django.db import models
 from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 from encrypted_fields.fields import EncryptedCharField
 
 from core.apps.authentication.choices import Sex, EducationLevel, Occupation
@@ -11,7 +12,7 @@ from core.apps.authentication.choices import Sex, EducationLevel, Occupation
 class CustomUserManager(BaseUserManager):
     def _create_user(self, email, password=None, **extra_fields):
         if not email:
-            raise ValueError('The email must be set')
+            raise ValueError(_('The email must be set'))
 
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.password = make_password(password)
